6bc8faf7d449e7d92089a7e54e37276375f39837
ruchi-s <ruchisingh700z@gmail.com>
2025-11-21 23:38:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/about", (req, res) => {
+  res.send("This is the about page")
+})
+
+app.get("/contact",(req,res) =>{
+    res.send("<h1>this is contact page</h1>")
+})
+
+app.get("/youtube",(req,res) =>{
+   res.send("youtube")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
